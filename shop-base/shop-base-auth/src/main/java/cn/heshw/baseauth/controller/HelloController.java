31978ff3da9998a6838bb1af923d31e48ebcb3af
He <g00qzzne@johndeere.com>
2020-12-20 16:22:32 +0800

    gateway配置完成

--- shop-base/shop-base-auth/src/main/java/cn/heshw/baseauth/controller/HelloController.java
+++ shop-base/shop-base-auth/src/main/java/cn/heshw/baseauth/controller/HelloController.java
@@ -1,6 +1,5 @@
 package cn.heshw.baseauth.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class HelloController {
-
-  @Value(value = "${name}")
-  private String moduleName;
-
-  public void setModuleName(String moduleName) {
-    this.moduleName = moduleName;
-  }
+//
+//  @Value(value = "${name}")
+//  private String moduleName;
+//
+//  public void setModuleName(String moduleName) {
+//    this.moduleName = moduleName;
+//  }
 
   @GetMapping(value = "/auth/hello")
   public String hello() {
-    return "hello" + moduleName;
+    return "hello";
   }
 }
