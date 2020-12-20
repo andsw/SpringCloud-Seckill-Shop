80c4a08f5f479e0dbe9d06e69b7061dec1d42070
He <g00qzzne@johndeere.com>
2020-12-20 15:32:57 +0800

    nacos-config 功能

---
+++ shop-base/shop-base-gateway/src/main/java/cn/heshw/basegateway/ShopBaseGatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.heshw.basegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ShopBaseGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShopBaseGatewayApplication.class, args);
+  }
+}
