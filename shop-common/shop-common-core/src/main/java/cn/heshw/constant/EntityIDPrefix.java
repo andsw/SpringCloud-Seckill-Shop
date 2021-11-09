42d602f43aba6d23a487cadb00c29910f00106ca
He <g00qzzne@johndeere.com>
2021-11-09 23:16:03 +0800

    IdGenerator done

---
+++ shop-common/shop-common-core/src/main/java/cn/heshw/constant/EntityIDPrefix.java
@@ -0,0 +1,19 @@
+package cn.heshw.constant;
+
+public enum EntityIDPrefix {
+  SKU("SKU"), SPU("SPU");
+
+  private String prefix;
+
+  EntityIDPrefix(String prefix) {
+    this.prefix = prefix;
+  }
+
+  public String getPrefix() {
+    return prefix;
+  }
+
+  public void setPrefix(String prefix) {
+    this.prefix = prefix;
+  }
+}
---
+++ shop-common/shop-common-util/src/main/java/cn/heshw/id/IDGenerator.java
@@ -0,0 +1,100 @@
+package cn.heshw.id;
+
+import cn.heshw.constant.EntityIDPrefix;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import javax.validation.constraints.NotNull;
+
+public class IDGenerator {
+
+  private static final int RANDOM_BOUND = 4096;
+
+  // timestamp start from 2021-01-01 00:00:00
+  private static final long START_TIMESTAMP = 1609430400;
+  private static final int SERVER_ID_SHIFT = 22;
+  private static final int TIMESTAMP_SHIFT = 12;
+  private static final int MAX_TAIL_ID = 4096;
+
+  private Long lastTimeSec;
+  private Integer tailIncreaseId;
+  private Integer serverId;
+
+  public IDGenerator(int serverId) {
+    this.serverId = serverId;
+    lastTimeSec = currentTimeSec();
+    tailIncreaseId = 0;
+  }
+
+  public synchronized String generateIdFor(EntityIDPrefix entityIDPrefix) {
+    return generateIdFor(entityIDPrefix.getPrefix());
+  }
+
+  public synchronized String generateIdFor(String entityType) {
+    long current = currentTimeSec();
+
+    if (current != lastTimeSec) {
+      lastTimeSec = current;
+      tailIncreaseId = 0;
+    } else {
+      if (tailIncreaseId >= MAX_TAIL_ID) {
+        waitForNextSec();
+        lastTimeSec = currentTimeSec();
+        tailIncreaseId = 0;
+      }
+    }
+    return entityType + snowFlake();
+  }
+
+  private long snowFlake() {
+//  64位数： 1正负，41位时间戳，10位当前服务器id，12位生成的随机数范围[0, 2^ 12 - 1]
+    long res = 0;
+    res = res | (lastTimeSec << TIMESTAMP_SHIFT);
+    res = res | (serverId << SERVER_ID_SHIFT);
+    res = res | (tailIncreaseId++);
+//    System.out.println(Long.toBinaryString(res));
+    return res;
+  }
+
+  private long currentTimeSec() {
+    return (System.currentTimeMillis() - START_TIMESTAMP) / 1000;
+  }
+
+  private void waitForNextSec() {
+    long currentSec = currentTimeSec();
+    while (currentSec != lastTimeSec) {
+      currentSec = currentTimeSec();
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    Set<String> set = new HashSet<>();
+    IDGenerator generator = new IDGenerator(1);
+    ExecutorService service = Executors.newFixedThreadPool(5, new ThreadFactory() {
+      private int no = 0;
+      @Override
+      public Thread newThread(@NotNull Runnable r) {
+        return new Thread(r, (no++) + "NO");
+      }
+    });
+    for (int j = 0; j < 100; j++) {
+      service.submit(() -> {
+        for (int i = 0; i < 10; i++) {
+          String id = generator.generateIdFor(Thread.currentThread().getName());
+          if (set.contains(id)) {
+            System.out.println(id);
+          } else {
+            set.add(id);
+          }
+        }
+      });
+    }
+    service.shutdown();
+    if (service.awaitTermination(360, TimeUnit.SECONDS)) {
+      System.out.println(set.size());
+    }
+  }
+}
